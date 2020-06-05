0a4e8c408b3b29eaffc12550b606b9d964069de6
Sanyam Jain <Sj02.9626@gmail.com>
2020-06-06 02:57:52 +0530

    fix

--- Exercise_1/main.tf
+++ Exercise_1/main.tf
@@ -1,7 +1,7 @@
 # TODO: Designate a cloud provider, region, and credentials
 provider "aws" {
-  access_key = "access_key"
-  secret_key = "secret_key"
+  access_key = ""
+  secret_key = ""
   region = "us-east-1"
 }
 
@@ -12,6 +12,9 @@ resource "aws_instance" "Udacity_T2" {
   ami = "ami-085925f297f89fce1"
   instance_type = "t2.micro"
   count = 4
+  tags = {
+    Name = "Udacity_T2"
+  }
 }
 
 # TODO: provision 2 m4.large EC2 instances named Udacity M4
@@ -19,4 +22,7 @@ resource "aws_instance" "Udacity_M4" {
   ami = "ami-085925f297f89fce1"
   instance_type = "m4.large"
   count = 2
+  tags = {
+    Name = "Udacity_M4"
+  }
 }
--- Exercise_2/main.tf
+++ Exercise_2/main.tf
@@ -1,7 +1,7 @@
 #Intitalize Credentials
 provider "aws" {
-  access_key = "access_key"
-  secret_key = "secret_key"
+  access_key = ""
+  secret_key = ""
   region = "us-east-1"
 }
 
@@ -11,7 +11,50 @@ data "archive_file" "lambda" {
   source_file = "greet_lambda.py"
 }
 
-data "aws_iam_policy_document" "lambda_policy" {
+resource "aws_iam_role" "iam_lambda" {
+  name = "iam_lambda"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_policy" "lambda_log" {
+  name        = "lambda_log"
+  path        = "/"
+  description = "IAM policy for logging from a lambda"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Resource": "arn:aws:logs:*:*:*",
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
+data "aws_iam_policy_document" "policy_lambda" {
   statement {
     sid    = ""
     effect = "Allow"
@@ -25,23 +68,23 @@ data "aws_iam_policy_document" "lambda_policy" {
   }
 }
 
-resource "aws_cloudwatch_log_group" "logs_lambda" {
+resource "aws_cloudwatch_log_group" "lambda_policy_cloudwatch" {
   name              = "/aws/lambda/greet_lambda"
   retention_in_days = 14
 }
 
-resource "aws_iam_role" "lambda_iam" {
-  name               = "lambda_iam"
-  assume_role_policy = data.aws_iam_policy_document.lambda_policy.json
+resource "aws_iam_role_policy_attachment" "lambda_policy" {
+  role       = aws_iam_role.iam_lambda.name
+  policy_arn = aws_iam_policy.lambda_log.arn
 }
 
 resource "aws_lambda_function" "greet_lambda" {
   filename         = "greet_lambda.zip"
   function_name    = "greet_lambda"
+  role       = aws_iam_role.iam_lambda.arn
   handler          = "greet_lambda.lambda_handler"
-  role             = aws_iam_role.lambda_iam.arn
   source_code_hash = data.archive_file.lambda.output_base64sha256
-  depends_on       = [aws_cloudwatch_log_group.logs_lambda]
+  depends_on       = [aws_cloudwatch_log_group.lambda_policy_cloudwatch]
 
   runtime = var.runtime
   environment {
